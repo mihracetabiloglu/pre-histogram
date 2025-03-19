fbbda4a5b8b44f80ceea4b5a8c3b64cad9cf2512
irmakyuceak <irmakyuceak0803@gmail.com>
2025-03-19 14:59:21 +0300

    feat: Add Equalization Executor and Update Package Model

--- apps/export.py
+++ apps/export.py
@@ -2,7 +2,7 @@ import os
 import sys
 import json
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../'))
-from components.pre_histogram.src.models.PackageModel import PackageModel
+from components.Histogram.src.models.PackageModel import PackageModel as Package
 
 with open("data.json", "w") as f:
     f.write(Package.schema_json(indent=2))

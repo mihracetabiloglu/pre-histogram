11002eae77e5be9495f5aa685943204febdcf292
irmakyuceak <irmakyuceak0803@gmail.com>
2025-03-28 16:22:15 +0300

    initialize commit

--- src/executors/Equalization.py
+++ src/executors/Equalization.py
@@ -1,59 +1,82 @@
 import os
 import sys
 import cv2
+import numpy as np
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
 
 from sdks.novavision.src.media.image import Image
-from sdks.novavision.src.base.model import Image as ImageModel
 from sdks.novavision.src.base.response import Response
 from sdks.novavision.src.base.component import Component
 from sdks.novavision.src.helper.executor import Executor
-from components.Histogram.src.utils.response import build_response
+from components.Histogram.src.utils.response import build_response_equalization
 from components.Histogram.src.models.PackageModel import PackageModel
 
 
 class Equalization(Component):
     """
-    This executor applies histogram equalization to grayscale or RGB images
-    using OpenCV's equalizeHist() function.
+    This class applies CLAHE (Contrast Limited Adaptive Histogram Equalization) to an image.
+
+    Attributes
+    ----------
+    request : object
+        request parameters
+
+    Methods
+    -------
+    apply_clahe():
+        Applies CLAHE to the input image.
     """
 
     def __init__(self, request, bootstrap):
-        super().__init__(request)
+        super().__init__(request, bootstrap)
         self.request.model = PackageModel(**(self.request.data))
-        self.initialize_request_data(request=request, bootstrap=bootstrap)
         self.image = self.request.get_param("inputImage")
-        self.equalize_gray = self.request.get_param("configEqualizationGrayScale") == "True"
-        self.equalize_rgb = self.request.get_param("configEqualizationRGB") == "True"
-        self.plot_histogram = self.request.get_param("configPlotHistogram") == "True"
+        self.clip_limit = self.request.get_param("clip_limit")
+        self.tile_grid_size = self._parse_tile_size(self.request.get_param("tile_grid_size"))
+        self.convert_to_gray = self.request.get_param("convert_to_gray")
 
     @staticmethod
     def bootstrap() -> dict:
         return {}
 
-    def run(self):
-        img = Image.get_frame(img=self.image, redis_db=self.redis_db)
-        if not img: return None
+    def _parse_tile_size(self, tile_size_str):
+        return tuple(map(int, tile_size_str.strip("()").split(",")))
 
-        processed_img = self.apply_equalization(img.value)
-        output_image = Image.set_frame(img=Image(value=processed_img), package_uID=self.uID, redis_db=self.redis_db)
-        packageModel = build_response(context=self)
-        packageModel.outputs = {"outputImage": output_image}
+    def apply_clahe(self, image):
+        if len(image.shape) == 3 and image.shape[2] == 3:
+            if str(self.convert_to_gray) == "True":
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            else:
+                raise ValueError(
+                    "CLAHE yalnızca tek kanallı (grayscale) görüntü üzerinde çalışır. "
+                    "Lütfen 'convert_to_gray' ayarını True yapınız veya grayscale görüntü gönderiniz."
+                )
+
+        if len(image.shape) != 2:
+            raise ValueError("CLAHE uygulanacak görüntü tek kanallı olmalıdır!")
+
+        if image.dtype != np.uint8:
+            image = np.clip(image, 0, 255)
+            image = image.astype(np.uint8)
+
+        clahe = cv2.createCLAHE(clipLimit=self.clip_limit, tileGridSize=self.tile_grid_size)
+        return clahe.apply(image)
+
+    def run(self):
+        """
+        Executes the CLAHE histogram equalization process.
+        """
+        img = Image.get_frame(img=self.image, redis_db=self.redis_db)
+        if not img:
+            return None
+
+        img.value = self.apply_clahe(img.value)
+
+        self.image = Image.set_frame(img=img, package_uID=self.uID, redis_db=self.redis_db)
+        packageModel = build_response_equalization(context=self)
         return Response(model=packageModel, bootstrap=self.bootstrap).response()
 
-    def apply_equalization(self, image):
-        if self.equalize_gray:
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            equalized_gray = cv2.equalizeHist(gray)
-            return cv2.cvtColor(equalized_gray, cv2.COLOR_GRAY2BGR)
 
-        if self.equalize_rgb:
-            img_yuv = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
-            img_yuv[:, :, 0] = cv2.equalizeHist(img_yuv[:, :, 0])
-            return cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
-
-        return image
-
-if "__main__" == __name__:
+if __name__ == "__main__":
     Executor(sys.argv[1]).run()
--- src/utils/response.py
+++ src/utils/response.py
@@ -1,14 +1,26 @@
 
 from sdks.novavision.src.helper.package import PackageHelper
-from components.pre_histogram.src.models.PackageModel import PackageModel, PackageConfigs, HistogramOutputs, HistogramResponse, HistogramExecutor, ConfigExecutor, OutputData, OutputImage
+from components.Histogram.src.models.PackageModel import PackageModel, PackageConfigs, HistogramOutputs, HistogramResponse, HistogramExecutor, EqualizationExecutor, EqualizationResponse, EqualizationOutputs, ConfigExecutor, OutputData, OutputImage
 
-def build_response(context):
+def build_response_histogram(context):
     outputData = OutputData(value=context.out)
     outputImage = OutputImage(value=context.image)
     Outputs = HistogramOutputs(outputData=outputData, outputImage=outputImage)
     histogramResponse = HistogramResponse(outputs=Outputs)
     histogramExecutor = HistogramExecutor(value=histogramResponse)
     executor = ConfigExecutor(value=histogramExecutor)
+    packageConfigs = PackageConfigs(executor=executor)
+    package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
+    packageModel = package.build_model(context)
+    return packageModel
+
+
+def build_response_equalization(context):
+    outputImage = OutputImage(value=context.image)
+    Outputs = EqualizationOutputs(outputImage=outputImage)
+    equalizationResponse = EqualizationResponse(outputs=Outputs)
+    equalizationExecutor = EqualizationExecutor(value=equalizationResponse)
+    executor = ConfigExecutor(value=equalizationExecutor)
     packageConfigs = PackageConfigs(executor=executor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)

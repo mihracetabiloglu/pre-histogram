7a0508c4b9d79129957922c1e8984b9c9f3a38b3
Mihrace <mihracetabiloglu@gmail.com>
2026-08-10 10:51:20 +0300

    matplotlib.use('Agg) import olarak eklendi

--- src/executors/HistogramExecutor.py
+++ src/executors/HistogramExecutor.py
@@ -2,6 +2,8 @@ import os
 import sys
 import cv2
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
